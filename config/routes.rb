9d3927b43f5216312733dd64505192df4607b729
Maan Patel <ma23pate@uwaterloo.ca>
2022-02-05 11:10:37 -0500

    Edit button for Feedback now works

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   get 'regenerate_admin_code', to: 'options#regenerate_admin_code'
   get 'reset_password', to: 'static_pages#show_reset_password'
   post 'reset_password', to: 'static_pages#reset_password'
-        
+  get 'feedbacks/:id/edit', to: 'feedbacks#edit', as: "feedback_edit"
+
   get 'teams/:id/confirm_delete_user_from_team', to: 'teams#confirm_delete_user_from_team', as: 'team_confirm_delete_delete_user_from_team'  
   get 'teams/:id/confirm_delete', to: 'teams#confirm_delete', as: 'team_confirm_delete'
   resources :teams 
@@ -21,6 +22,7 @@ Rails.application.routes.draw do
   post   '/login',  to: 'sessions#create'    
   get    '/logout', to: 'sessions#destroy' 
   get '/signup', to: 'users#new'
+
   resources :users, except: [:new]
   resources :options do
     post :toggle_reports
@@ -28,6 +30,9 @@ Rails.application.routes.draw do
   resources :teams do
     post :remove_user_from_team
   end  
+
+  patch 'feedbacks/:id', to: 'feedbacks#update'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 

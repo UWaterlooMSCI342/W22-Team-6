c97b1dcf49398eeb9f3cb41bfd55124eecc3e46a
Maan Patel <ma23pate@uwaterloo.ca>
2022-02-23 14:05:41 -0500

    Moved status function from User model to Feedback, and fixed tests

--- test/system/sprint4/visual_indicators_test.rb
+++ test/system/sprint4/visual_indicators_test.rb
@@ -7,29 +7,40 @@ require "application_system_test_case"
 class VisualIndicatorsTest < ApplicationSystemTestCase
   setup do
     @prof = User.create(email: 'charles@gmail.com', password: 'banana', password_confirmation: 'banana', name: 'Charles', is_admin: true)
-    
+    @prof2 = User.create(email: 'msmucker@gmail.com', password: 'banana', password_confirmation: 'banana', name: 'Mark', is_admin: true)
+
     @user1 = User.create(email: 'charles2@gmail.com', password: 'banana', password_confirmation: 'banana', name: 'Charles1', is_admin: false)
     @user1.save!
     @user2 = User.create(email: 'charles3@gmail.com', password: 'banana', password_confirmation: 'banana', name: 'Charles2', is_admin: false)
     @user2.save!
     @user3 = User.create(email: 'charles4@gmail.com', password: 'banana', password_confirmation: 'banana', name: 'Charles3', is_admin: false)
+    @user3.save!
+
     @team = Team.new(team_code: 'Code', team_name: 'Team 1')
     @team.users = [@user1, @user2]
+    @team2 = Team.new(team_code: 'Test', team_name: 'Test 2')
+    @team2.users = [@user3]
+
     @team.user = @prof 
-    @team.save!     
+    @team.save!
+    @team2.user = @prof2
+    @team2.save!
 
     @feedback = save_feedback(1,1,1, "This team is disorganized", @user1, DateTime.civil_from_format(:local, 2021, 1, 20) - 7, @team)
     @feedback2 = save_feedback(5,5,5, "This team is disorganized", @user2, DateTime.civil_from_format(:local, 2021, 1, 20), @team)
     @feedback3 = save_feedback(5,5,5, "This team is disorganized", @user1, DateTime.civil_from_format(:local, 2021, 1, 20), @team)
+    @feedback4 = save_feedback(1,1,1, "This team is disorganized", @user3, DateTime.now, @team2)
+  
   end 
   
-  def test_student_view 
+  # The current week hasn't been submitted.
+  def test_student_view_no_submission_on_current_week 
     visit root_url 
     login 'charles2@gmail.com', 'banana'
   
-    # assert_text "status"
+    # We have a blue status. 
     within('#' + @user1.id.to_s + '-status') do
-      assert find('.dot.red')
+      assert find('.dot.blue')
     end
     
     click_on 'View Historical Data'
@@ -41,6 +52,17 @@ class VisualIndicatorsTest < ApplicationSystemTestCase
       assert find('.dot.red')
     end
   end
+
+  # The current week has now been submitted. 
+  def test_student_view_submission_on_current_week
+    visit root_url 
+    login 'charles4@gmail.com', 'banana'
+
+    within('#' + @user3.id.to_s + '-status') do
+      assert find('.dot.red')
+    end
+    
+  end
   
   def test_professor_view 
     visit root_url 
@@ -50,7 +72,7 @@ class VisualIndicatorsTest < ApplicationSystemTestCase
       assert find('.dot.red')
     end 
     
-    click_on 'Details'
+    click_on 'Details', match: :first
     
     within('#2021-3') do 
       assert find('.dot.green')

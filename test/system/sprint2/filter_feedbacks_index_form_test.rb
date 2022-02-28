2b06e25f56b28c4282d7e8d639732b36ad5dd6ba
Gillian Tsoi <gtsoi@uwaterloo.ca>
2022-02-27 19:57:29 -0500

    add tests

--- test/system/sprint2/filter_feedbacks_index_form_test.rb
+++ test/system/sprint2/filter_feedbacks_index_form_test.rb
@@ -22,10 +22,33 @@ class FilterFeedbacksIndexFormTest < ApplicationSystemTestCase
     select "5", from: "Effort rating"
     select "5", from: "Punctuality rating"
     select "Low", from: "Priority"
-    fill_in "Start date", with: "2022/01/18"
-    fill_in "End date", with: "2022/01/23"
+    fill_in "Start date", with: "2022-01-18"
+    fill_in "End date", with: "2022-01-23"
     click_on "Filter"
 
+    assert_text "Applied filters:"
+    assert_text "First Name: User1"
+    assert_text "Last Name: User1"
+    assert_text "Team: Team1"
+    assert_text "Participation Rating: 5"
+    assert_text "Effort Rating: 5"
+    assert_text "Punctuality Rating: 5"
+    assert_text "Priority: Low"
+    assert_text "Timestamp: 2022-01-18 to 2022-01-23"
     assert_text "User1 User1 Team1 5 5 5 Low First week went great! 2022-01-20 00:00 EST"
+  end
+
+  def test_filter_feedbacks_reset
+    visit root_url
+    login 'msmucker@gmail.com', 'professor'
+
+    click_on "Feedback & Ratings"
+
+    fill_in "First name", with: "User1"
+    click_on "Filter"
+    
+    click_on "Reset"
+
+    assert_current_path feedbacks_url
   end
 end

6e71040afeafa7e432f78eebf332db7e5d06ce6b
Gillian Tsoi <gtsoi@uwaterloo.ca>
2022-03-27 14:58:53 -0400

    add system test

--- test/system/sprint2/filter_feedbacks_index_form_test.rb
+++ test/system/sprint2/filter_feedbacks_index_form_test.rb
@@ -18,11 +18,11 @@ class FilterFeedbacksIndexFormTest < ApplicationSystemTestCase
     fill_in "First name", with: "User1"
     fill_in "Last name", with: "User1"
     select "Team1", from: "Team"
-    select "5", from: "participation_rating_start"
+    select "4", from: "participation_rating_start"
     select "5", from: "participation_rating_end"
-    select "5", from: "effort_rating_start"
+    select "4", from: "effort_rating_start"
     select "5", from: "effort_rating_end"
-    select "5", from: "punctuality_rating_start"
+    select "4", from: "punctuality_rating_start"
     select "5", from: "punctuality_rating_end"
     select "Low", from: "Priority"
     fill_in "Start date", with: "2022-01-18"
@@ -33,12 +33,37 @@ class FilterFeedbacksIndexFormTest < ApplicationSystemTestCase
     assert_text "First Name: User1"
     assert_text "Last Name: User1"
     assert_text "Team: Team1"
-    assert_text "Participation Rating: 5"
-    assert_text "Effort Rating: 5"
-    assert_text "Punctuality Rating: 5"
+    assert_text "Participation Rating: 4-5"
+    assert_text "Effort Rating: 4-5"
+    assert_text "Punctuality Rating: 4-5"
     assert_text "Priority: Low"
     assert_text "Timestamp: 2022-01-18 to 2022-01-23"
     assert_text "User1 User1 Team1 5 5 5 Low First week went great! 2022-01-20 00:00 EST"
+  end
+
+  def test_one_rating_dropdown_selected
+    visit root_url
+    login 'msmucker@gmail.com', 'professor'
+
+    # select start only
+    click_on "Feedback & Ratings"
+
+    select "4", from: "participation_rating_start"
+    select "4", from: "effort_rating_start"
+    select "4", from: "punctuality_rating_start"
+    click_on "Filter"
+
+    assert_no_text "Applied filters:"
+
+    # reset and select end only
+    click_on "Feedback & Ratings"
+
+    select "4", from: "participation_rating_end"
+    select "4", from: "effort_rating_end"
+    select "4", from: "punctuality_rating_end"
+    click_on "Filter"
+
+    assert_no_text "Applied filters:"
   end
 
   def test_filter_feedbacks_reset

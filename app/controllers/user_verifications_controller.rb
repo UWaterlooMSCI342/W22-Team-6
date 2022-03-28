83d9641bb9da78bfcb30ab8c363598d250c07b34
Troy Zada <tzada@uwaterloo.ca>
2022-03-28 18:34:26 -0400

    feat(before_action): added before_action to controller such that only permissible users can access user_verifications

--- app/controllers/user_verifications_controller.rb
+++ app/controllers/user_verifications_controller.rb
@@ -1,4 +1,11 @@
 class UserVerificationsController < ApplicationController
+  before_action :require_login
+  before_action :require_admin
+
+  def index
+    @user_verifications = UserVerification.paginate(page: params[:page], per_page: 10)
+  end
+
   # https://www.mattmorgante.com/technology/csv
   def import
     # Assumes CSV file imported is correctly formatted.
@@ -10,9 +17,5 @@ class UserVerificationsController < ApplicationController
       redirect_to user_verifications_url
       flash[:error] = "#{exception} for a row."
     end
-  end
-
-  def index
-    @user_verifications = UserVerification.paginate(page: params[:page], per_page: 10)
   end
 end
